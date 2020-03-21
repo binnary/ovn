1d1c17da917e8dbe8a0eaa2c8ff083e0c8d57027
binnary <binnary@foxmail.com>
2020-03-21 13:38:05 +0800

    init

--- ovn/urls.py
+++ ovn/urls.py
@@ -13,9 +13,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+<<<<<<< HEAD
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+=======
+import xadmin
+#from django.contrib import admin
+from django.urls import path
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    #path('admin/', admin.site.urls),
+    path(r'', xadmin.site.urls),
+>>>>>>> 802375e... init
 ]
